3111a24ea9f77b7d7053bbcb007d5bf0e0764770
Jason <jiameng@jiamengdeMacBook-Air.local>
2018-11-07 02:58:26 +0800

    首页添加效果

--- js/index.js
+++ js/index.js
@@ -75,3 +75,148 @@
 })(750, 750);
 
 
+
+
+
+/**
+ * tag 选择组件
+ * @param tagItem           tag匹配的数组，注数组顺序要与页面显示顺序一致
+ * @param itemBoxId         tag标签外侧div的id  默认‘tag’
+ * @param tagItemClassName  tag标签的class 默认‘tagItem’
+ * @param returnTagDataId   tag标签需要set返回值的input的id 默认‘returnTagDataId’
+ * @param selectClass       选中标签的高亮class 默认‘tagHover’
+ * @param isRadio           是否是单选          默认false
+ * @param radioDefault      选中标签的高亮class
+ * @param rowData           用于首页二级
+ */
+function getSelectData(tagItem, itemBoxId, tagItemClassName, returnTagDataId, selectClass, isRadio, radioDefault, rowData) {
+    if(!tagItem)   tagItem = [];
+    if(!itemBoxId) itemBoxId = 'tag';
+    if(!itemBoxId) itemBoxId = 'tagItem';
+    if(!returnTagDataId) returnTagDataId = 'returnTagDataId';
+    if(!selectClass) selectClass = 'tagHover';
+    if(!isRadio) isRadio = false;
+    if(!radioDefault) radioDefault = null;
+    var returnData=[];
+    var tagEle = $('#'+itemBoxId + ' .' + tagItemClassName);
+
+
+    if(isRadio && radioDefault){
+        //根据当前选中tag匹配默认值  设置默认值及默认选中class
+        var s = tagItem.join(",").indexOf(radioDefault);
+        $("#"+returnTagDataId).val(radioDefault);
+        $(tagEle[s]).addClass(selectClass);
+    }else{
+        $("#"+returnTagDataId).val(tagItem[0]);
+        $(tagEle[0]).addClass(selectClass);
+    }
+
+    //给每一个tag绑定点击事件
+    for(var i = 0,m = tagEle.length; i < m;i++){
+        $(tagEle[i]).click(function(){
+            
+            // 判断标签是否是单选
+            if(isRadio){
+                //获取当前点击tag的id
+                var _thisI = $(this).attr('data-id');
+                console.info('_thisI', _thisI)
+                tagEle.each(function () {
+                    $(this).removeClass(selectClass);
+                })
+                $(tagEle[_thisI]).addClass(selectClass);
+                //设置当前选中的值
+                $("#"+returnTagDataId).val($(tagEle[_thisI]).html());
+
+
+                //首页筛选二级单独处理
+                if(itemBoxId == 'row1'){
+                    $('#row2').remove();
+                    if(rowData[_thisI].child)
+                    addRow(rowData[_thisI].child, 1, 2);
+                }
+
+            }else{
+                //获取当前点击tag的id
+                var _thisI = $(this).attr('data-id');
+                console.info('_thisI', _thisI)
+                $(tagEle[_thisI]).addClass(selectClass)
+                //根据当前选中tag匹配是否已选
+                var s = returnData.join(",").indexOf(tagItem[_thisI]);
+                if(s>=0){
+                    for(var r in returnData){
+                        if( returnData[r] == tagItem[_thisI] ){
+                            returnData.splice(r,1);
+                            $(tagEle[_thisI]).removeClass(selectClass)
+                        }
+                    }
+                } else{
+                    returnData.push(tagItem[_thisI])
+                }
+                console.info('returnData' ,returnData);
+                $("#"+returnTagDataId).val(returnData.join(" "));
+            }
+
+        })
+    }
+}
+
+
+
+
+// 区域切换
+function showTab(type){
+    var tabItemEle = $('.showTabItem');
+    tabItemEle.removeClass('shover');
+    $('#screenType'). val($('#'+type).html());
+    $('.addRow').remove();
+    if(type == 'near'){
+        $(tabItemEle[0]).addClass('shover');
+        addRow(rentNear, 1, 1);
+    }else if(type == 'area'){
+        $(tabItemEle[1]).addClass('shover');
+        addRow(areaData, 2, 1);
+    }else if(type == 'subWay'){
+        $(tabItemEle[2]).addClass('shover');
+        addRow(subWayData, 2, 1);
+    }
+}
+
+function addRow(rowData, addNum,rowNum){
+    var jkScreenItem = '<div class="addRow jk-screen-cont jkFlexItem screenBg" id="row'+rowNum+'">'+
+                            '<ul class="jk-screen-ui">';
+    var tagList = [];
+    // 判断需要追加几行
+    if(addNum == 1){
+        for(var i=0; i<rowData.length; i++){
+            jkScreenItem += '<li class="tagItem" data-id="'+i+'">'+rowData[i]+'</li>';
+        }
+    }else{
+        for(var i=0; i<rowData.length; i++){
+            tagList.push(rowData[i].name);
+            jkScreenItem += '<li class="tagItem" data-id="'+i+'">'+rowData[i].name+'</li>';
+        }
+    }
+    
+    console.info('tagList', tagList);
+    jkScreenItem +='</ul>'+
+                    '<input type="hidden" name="row'+rowNum+'Val" id="row'+rowNum+'Val" />'+
+                    '</div>';
+    $('#jkScreenArea').append(jkScreenItem);
+
+    
+    if(addNum == 1){
+        getSelectData(rowData, 'row'+rowNum, 'tagItem', 'row'+rowNum+'Val','shover', true);
+    }else if(addNum==2 ){
+
+        getSelectData(tagList, 'row'+rowNum, 'tagItem', 'row'+rowNum+'Val','shover', true, undefined, rowData);
+
+        //根据当前选中tag匹配是否已选
+        var val = $('#row'+rowNum+'Val').val();
+        var s = tagList.join(",").indexOf(val);
+
+        if(s >= 0 && rowData[s].child && rowData[s].child.length>0){
+            addRow(rowData[s].child, 1, 2);
+        }
+    }
+}
+
